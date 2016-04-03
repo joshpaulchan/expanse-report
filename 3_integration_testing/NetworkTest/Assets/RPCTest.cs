f94799b389071d5a8d6420c5efbaf23e951b9e09
aw449 <awong7905@gmail.com>
2016-04-03 16:22:45 -0400

    networktest

---
+++ 3_integration_testing/NetworkTest/Assets/RPCTest.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[AddComponentMenu("Cardboard/CardboardHead")]
+public class RPCTest : MonoBehaviour
+{
+    Quaternion r;
+    Vector3 p;
+
+    void Update()
+    {
+        //This is owned by the server
+       if(networkView.isMine)
+       {
+
+        networkView.RPC("CmdCameraup",RPCMode.All,newobject[] {r,q});
+       }
+       //This is the client, assumed to be the smartphone
+       else{
+        Cardboard.SDK.UpdateState();
+        //Get Rotation data
+        r = Cardboard.SDK.HeadPose.Orientation;
+        p= Cardboard.SDK.HeadPose.Position;
+
+        CmdCameraup(p, r);
+       }
+       //The RPC function
+       [RPC]
+       public void CmdCameraup(Vector3 pos,Quaternion Rot){
+            this.transform.localRotation =  Rot;
+       }
+}
